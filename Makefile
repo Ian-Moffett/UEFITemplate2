32fc4b64ea100b4f31e8d45bde5f645011bf2415
Ian-Moffett <hscorp99906@gmail.com>
2022-03-11 21:49:09 -0500

    Improved.

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
 all:
+	cd src/kernel; rm -rf limine 2> /dev/null; make clean; make
+	cd src/kernel; bash build.sh; mv Omega.iso ../../
+
+start:
 	git clone https://github.com/limine-bootloader/limine.git --branch=v2.0-branch-binary --depth=1 src/kernel/limine
-	cd src/kernel; make; bash build.sh; mv Omega.iso ../../
+	make
 
 reset:
 	cd src/kernel; rm -rf limine; make clean
+
+
+run:
+	qemu-system-x86_64 -cdrom Omega.iso
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,4 +2,19 @@
 #include <stddef.h>
 #include "stivale2.h"
 
+void* get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
+    struct stivale2_tag* curTag = (void*)stivale2_struct->tags;
 
+    while (1) {
+        if (!(curTag)) {
+            // End.
+            return NULL;
+        }
+
+        if (curTag->identifier == id) {
+            return curTag;
+        }
+
+        curTag = (void*)curTag->next;
+    }
+}
